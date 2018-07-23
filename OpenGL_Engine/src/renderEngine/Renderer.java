6d4bc0d2b59ad7511a56ec292cba9978699934a2
Kunal Chandan <kunal_chandan@outlook.com>
2018-07-23 02:12:33 -0400

    Completed Tutorial 2
    This still does not render the rectangle yet, This should probably be resolved by Tutorial 5 once shaders are added

---
+++ OpenGL_Engine/src/renderEngine/Renderer.java
@@ -0,0 +1,20 @@
+package renderEngine;
+
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL20;
+import org.lwjgl.opengl.GL30;
+
+public class Renderer {
+	private static float[] bg = {0.7f,0.9f,0.6f,1};
+	public void prepare() {
+		GL11.glClearColor(bg[0], bg[1], bg[2], bg[3]);
+	}
+	
+	public void render(RawModel model) {
+		GL30.glBindVertexArray(model.getVaoID());
+		GL20.glEnableVertexAttribArray(0);
+		GL11.glDrawArrays(GL11.GL_TRIANGLES, 0, model.getVertexCount());
+		GL20.glDisableVertexAttribArray(0);
+		GL30.glBindVertexArray(0);
+	}
+}
